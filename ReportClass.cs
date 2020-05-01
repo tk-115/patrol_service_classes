859cd95b38934ac47862fc1f47384a1aa8a020e5
tk-115 <58888857+tk-115@users.noreply.github.com>
2020-05-01 22:46:35 +0300

    Create ReportClass.cs

---
+++ ReportClass.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Patrol_service
+{
+    public class ReportClass
+    {
+        public DateTime Start, Finish;
+        public int Proto_count, Crew_Bort_num, Track_ID;
+        public string Sotr1_FIO, Sotr2_FIO, Sotr3_FIO, Sotr1_rank, Sotr2_rank, Sotr3_rank;
+    }
+}
