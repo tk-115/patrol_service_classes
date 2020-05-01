1f7f7eebe5da796982240c9a198e5e28488e6137
tk-115 <58888857+tk-115@users.noreply.github.com>
2020-05-01 22:47:34 +0300

    Create WitnessesClass.cs

---
+++ WitnessesClass.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Patrol_service
+{
+    public class WitnessesClass
+    {
+        public WitnessesClass() { }
+        public string FIO, pidpfilename;
+    }
+}
