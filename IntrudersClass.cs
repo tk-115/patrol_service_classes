4c1c1a8c05317a93c66f957c988c728e474eb60d
tk-115 <58888857+tk-115@users.noreply.github.com>
2020-05-01 22:45:59 +0300

    Create IntrudersClass.cs

---
+++ IntrudersClass.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Patrol_service
+{
+    public class IntrudersClass
+    {
+        public IntrudersClass() { }
+        public string FIO, pidpfilename;
+    }
+}
