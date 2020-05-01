d1252160bb2667559ee2436bc1f4d091b792b8ab
tk-115 <58888857+tk-115@users.noreply.github.com>
2020-05-01 22:46:17 +0300

    Create ProtocolClass.cs

---
+++ ProtocolClass.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Patrol_service
+{
+    public class ProtocolClass
+    {
+        public string Sotr_FIO, Sotr_rank, Intruder_FIO, Witnesses1_FIO, Witnesses2_FIO;
+        public string SOTR_pidpfilepath, Intruder_pidpfilepath, Witness1_pidpfilepath, Witness2_pidpfilepath;
+        public float Article;
+        public DateTime Date;
+    }
+}
